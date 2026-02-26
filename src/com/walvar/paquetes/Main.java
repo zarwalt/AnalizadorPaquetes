76e798fdfec9268274f883fc771595e18461fe51
user.email <zarwalt_dsd@192.168.1.7>
2026-02-25 20:53:11 -0600

    Refactorización: arreglos, for clásico y enhanced, manejo de errores.

--- src/com/walvar/paquetes/Main.java
+++ src/com/walvar/paquetes/Main.java
@@ -3,16 +3,24 @@ package com.walvar.paquetes;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
 			//prueba de formato a leer: Linea correcta --> Program_X:5.2_Y:3.1_ram
-			var paquete = AnalizadorPaquetes.coordenadas("Program_X:5.2_Y:3.1_ram");
-			System.out.println("Las coordenas son X: %.2f , Y: %.2f ".formatted(paquete.getCoordenadaX(), paquete.getCoordenadaY()));
-		} catch (IllegalArgumentException exception) {
-			System.out.println("No se pudo procesar la solicitud");
-			System.out.println("Motivo: %s ".formatted(exception.getMessage()));
+						
+		String[] paquetes = {"Program_X:5.2_Y:2.1_ram", "Program_X:3.2_Y:1.1_ram",
+				"Program_X:6.2_Y:4.1_ram", "Trogram_X:7.2_Y:8.1_ram",
+				"Program_X:1.2_Y:9.1_ram", "Program_X:4.2_Y:2.1_ramn"};
+			
+		for (int i = 0; i < paquetes.length; i++) {
+			System.out.println("Posicion: %d, Texto: %s".formatted(i, paquetes[i]));
 		}
-		
-		
+			
+		for (String paquete : paquetes) {
+			try {
+				var cadena = AnalizadorPaquetes.coordenadas(paquete);
+				System.out.println("Las coordenadas de %s, son X: %.2f - Y: %.2f".formatted(paquete, cadena.getCoordenadaX(), cadena.getCoordenadaY()));
+			} catch (IllegalArgumentException exception) {
+				System.out.println("No se pudo procesar la solicitud");
+				System.out.println("Motivo: %s ".formatted(exception.getMessage()));
+			}
+		}	
 	}
-
 }
